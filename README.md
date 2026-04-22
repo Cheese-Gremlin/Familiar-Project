9d89e8cda9a7a8e27a227c3c54bb23fe4c43916e
CheeseGremlin <afroitzheim153@gmail.com>
2026-04-23 01:08:31 +1200

    23/04
    -updated README
    -drew goober
    -added feelings
    -started menus
    -started goober changing colour depending on feeling

--- README.md
+++ README.md
@@ -1,2 +1,121 @@
 # Familiar-Project
  mddn 242 project 2
+
+AI used: Clause using Sonnet 4.6
+font used: https://fonts.google.com/selection/embed
+
+For simplicity i will call the familiar a goober until it has been given a 'shape'/ name
+
+Response to the brief:
+im very excited about this project. I think its a fun idea to create a little goober and designing it in a way that affects how the user feels/ interacts with the goober. Im also very excited about working in p5js again since I understand how it works (its where i learned my current coding skills so im comfy here) which means that I dont have to rely on and use AI too much throughout this project but can also do a lot myself. Yes using AI might make the process quicker but my goal is to learn how to code so im using this project as a learning opportunity and only using AI when i get stuck or take too much time on a task that it starts negatively affecting the timeline of my project.
+
+Notes:
+19/04
+    To be very honest i feel very discouraged about this project at the moment. I think the main issue is that the course isnt what i was hoping it to be, which was to learn how to code these things. I really just hate having to use AI and having the feeling that I rely on it because the actual coding isnt being taught and it isnt possible to do the project without using AI (unless i make my idea really small) because it will be too out of scope and take too much time. It is such a stupid dilemma and I really dont know how to deal with it. To be completely honest i just want to give up because this doesnt feel 'worth it'. I would rather spend my time learning how to do the code so that i know how it works and can understand everything rather than spending it making something with AI that I know I wont be happy with since i cant really use it to achieve my 'goal'. I also dont think i'll be using any of these projects in my portfolio because I am not at all proud of using AI to make things. I think the true value behind things is when human effort and care and knowledge has been put into a project. I do think that I wouldnt be feeling as strongly about this if I already knew how to do the steps and rather use AI to make it quicker so that i can focus on other stuff but because i know 'nothing' about what im actually doing/ what the AI is doing. 
+    My current plan is to just take a way simpler idea and see what i can do myself and still use this as a learning opportunity in some way so that i actually gain something from this rather than juts doing something just to hand it in for the project. My idea is to use AI to teach me how to do it but then i still do the actual coding myself using what the AI made as a guide for how different things work. I fins this quite a shame because it take the opportunity and job away from a real person who could teach me this but that doesnt seem to be a proper option at the moment so I will try to do the best out of what i have.
+    As much as I appreciate how cool AI could be and how it can be used to help us achieve more things, i really really hate the opportunities it is taking away from us, especially in a learning field.
+
+
+
+idea 1:
+I want to have a goober that lives in the PC of my website (from project 1) which helps the user be 'productive' by rewarding them for doing tasks. 
+![alt text](Images-ReadMe/Idea-1.png)
+However after a lot more thinking and iterating the idea ive realized that it is way too big for the scope of this project. I could scale it down by removing parts (like the built in word and other applications) but i wouldnt enjoy it as much since there are just so many more idea i have for it. I also worry that it will be too much like 'focus friend' or 'finch'. 
+
+
+idea 2:
+have a goober that responds to how the user is feeling and helps look after them
+![alt text](Images-ReadMe/Idea-2.png)
+
+Goober communicates to user through 'translator device' on screen and makes a small noise every time they say something --------------------------------------------------------------------
+
+When user presses on goober, goober asks how they can help:
+    - log a feeling
+        ⤷ goober encourages user and suggests things for user to do depending on the feeling
+        ⤷ goober changes colour depending on feeling, turns dark grey when feeling hasnt been loged in 6/8 hours
+    - log things they like to do
+        ⤷ Goober refers to this when suggesting things
+    - share food
+        ⤷ user tells goober what food they had (show picture?)
+        ⤷ goober could ask and log the users enjoyment of the food to refer to later
+        ⤷ brings down 'Hunger meter'
+    - ask goober for help
+        ⤷ task help: suggests things to do depending on users feeling,
+        ⤷ feeling down: goober compliments and encourages user to keep going and shares 'happy facts' to hopefully cheer up user, also asks if they want 'task help'
+        ⤷ food help: suggests different meals (links recipes?) from 'goober existing data base' or from what user has added to 'food log'
+    - user shares that they are proud of themselves for something
+
+potential stuff:
+    -user can choose 2 emotions and have goober be a gradient?
+    - check calender 
+        ⤷ shows how user was feeling on certain days. when pressing on specific day, expands to show all feelings, what user did (suggestions followed), what food they had etc.
+    
+Want Meter:
+    -the higher it is the more goober wants to check in with user
+        ⤷ moves around more and 'calls out' to get users attention
+    -increases over time
+        ⤷ bumps up a bit when user doesnt like goobers suggestions
+        ⤷ increases quicker when food meter hits max
+    -decreases when user interacts with goober
+        ⤷ big decrease when user accepts/ dose goobers suggestion
+        ⤷ big decrease when food meter is reset
+
+Hunger Meter:
+    - starts going up at set time during the day e.g. at 8am for breakfast, 1pm for lunch and 6pm for dinner (these times could be set by the user to fit their habits)
+        ⤷ as meter goes up, goober starts to let user know that they are hungry with the frequency increasing as the meter incases. (goobers expression will also change and tummy grumbles can be heard)
+    - hits max 2 hours after starting
+        ⤷ when max is hit, goober starts to 'riot' for food, holding up a sign and pacing from side to side, making noise
+    - meter gets reset when user 'shares food' (by pressing on the goober and telling them what food they had), goober says thanks for sharing food and has happy expression. 
+
+List of feelings: (these could be customizable by user)
+    - tired (orange) #fccd77
+    - angry (red) #fc7777
+    - annoyed (magenta) #d677fc
+    - bored (deep purple) #9f77fc
+    - sad (blue) #66a2fd
+    - distracted (light blue) #80eafd
+    - overwhelmed (cyan) #4ec9a4
+    - panic/scared (dark green) #87c951
+    - excited (lime) #a5eb64
+    - happy (yellow) #effc77
+    - neutral/calm (white/cream) #fff5de
+
+
+Goobers States:
+    -Riot State:
+        ⤷ goober holds up a sign with what it wants and is pacing from side to side of screen, making lots of noise
+    -Reactions:
+        ⤷ contempt/ neutral face when goober is just floating about :)
+        ⤷ happy face- when user interacts with goober :D
+        ⤷ sad face- when user doesnt accept goobers suggestions :(
+        ⤷ upset/ angry face- when in riot mode >:(
+    -Colour state:
+        ⤷ changes depending on how user is feeling
+        ⤷ goes dark grey when feelings arent logged in 6/8 hours 
+    -Curious state over 3 hours
+        ⤷ not curious = all normal
+        ⤷ 0.25 curious = makes noise every 20min
+        ⤷ 0.5 curious = comes close to screen and makes noise every 20min
+        ⤷ 0.75 curious = starts slowly walking from side to side, makes noise every 10min
+        ⤷ max curious = 'riot state' actives
+    -Hunger state over 2 hours 
+        ⤷ not hungry = all normal
+        ⤷ 0.25 hungry = lets user know they are hungry/ makes noise every 20min
+        ⤷ 0.5 hungry = tummy starts making grumbly noise every 20min. makes noise every 15min
+        ⤷ 0.75 hungry = starts slowly walking from side to side, tummy grumble every 10min, makes noise every 7min
+        ⤷ max hungry = 'riot state' actives
+
+
+
+Starting to work:
+    I was initially very overwhelmed and didnt know where to start. I tried to strip the given files down to bare bones so that I could make my own creation but it was really hard to tell what was needed and what wasnt and i kept getting really frustrated and almost panicky because i didnt know what almost any of the code meant/ did which made me feel very discouraged.
+
+    My 'solution' (which im not a big fan of but couldnt think of another way) was to ask Claude for help (since we are supposed to be working with AI anyway). I asked it to help me make some basic starting files:
+        Hi there!
+        I want to do a coding project. can you help me set up some basic files so that i can work from them easily? 
+        I want to have an html files that loads a canvas from a sketch.js which runs off p5js. I also want to have a style.css file which i can use to format/ style things.  
+        Please let me know if you need more information.
+    i then asked it to include the 'mobile' code from the files provided on nuku since i think that is important. 
+    ![alt text](<Images-ReadMe/AI created- basic start files.png>) <- rough screen shot of the files claude provided me with
+
+
--- sketch.js
+++ sketch.js
@@ -1,174 +1,412 @@
-// ============================================================
-//  YOUR CREATURE  —  sketch.js
-//  MDDN242 Project 2
-// ============================================================
+// sketch.js — p5.js starter
 
-new p5(function(p) {
+// Set to true if you want to reserve space for a side UI panel (360px wide)
+const SHOW_UI = false;
 
-    // ============================================================
-    //  SETTINGS
-    // ============================================================
+let textColour = [354, 0, 17];
 
-    // ============================================================
-    //  STATE
-    // ============================================================
+let showMainMenu = false
+let mainMenuOutlineColour = [42, 5, 92];
+let mainMenuFillColour = [42, 5, 99, 200];
+let mainMenuOutlineWidth = (3);
 
-    let creature;
-    // let micAnalyser = null;
-    // let micActive   = false;
-    // let micData     = null;
+let showFeelingsMenu = false
 
 
-    // ============================================================
-    //  CREATURE FACTORY
-    // ============================================================
 
-    function createCreature(x, y) {
-        return {
-            x, y,
-            originX: x,
-            originY: y,
-            need: 50, 
-        };
+let gooberOutlineColour = [0, 0, 100];
+let gooberFillColour = [0, 0, 100, 200];
+let outlineWidth = (5);
+let faceColour = [0, 0, 19];
+
+let gooberWidth = (220);
+let gooberHeight = (265);
+let gooberPosX = (150);
+let gooberPosY = (200);
+
+// goober expressions
+let contemptFace = true
+let happyFace = false
+let sadFace = false
+let angryFace = false
+
+// user feelings
+let tired = false           // #fccd77
+let angry = false            // #fc7777
+let annoyed = false         // #d677fc
+let bored = false           // #9f77fc
+let sad = false             // #66a2fd
+let distracted = false      // #80eafd
+let overwhelmed = false     // #4ec9a4
+let panicked = false        // #87c951
+let excited = false         // #b5eb64
+let happy = false           // #effc77
+let calm = false            // #fff8e8
+
+function preload(){
+  GooberImg =loadImage("images/Goober-Template.png")
+}
+
+function isMobile() {
+  return window.innerWidth <= 768;
+}
+
+function canvasSize() {
+  if (isMobile()) {
+    return { w: window.innerWidth, h: window.innerHeight };
+  }
+  return {
+    w: SHOW_UI ? windowWidth - 360 : windowWidth - 40,
+    h: windowHeight - 40,
+  };
+}
+
+function setup() {
+  const { w, h } = canvasSize();
+  createCanvas(w, h);
+  strokeCap(ROUND);
+  strokeJoin(ROUND);
+  colorMode (HSB); //same as HSV
+  textFont('Indie Flower');
+}
+
+function draw() {
+    background (245, 35, 67);
+    
+    drawGoober(gooberPosX, gooberPosY);
+    if (showMainMenu === true){
+        drawMainMenu(gooberPosX, gooberPosY);
+    }
+    if (showFeelingsMenu === true){
+        drawFeelingsMenu(gooberPosX, gooberPosY);
+    }
+}
+
+function drawGoober(x, y){
+    //image (GooberImg, x, y, 256, 311); //template for drawing goober using code
+    gooberColour(200, 250); //opacity of body fill colour, opacity of line colour
+//body
+    fill(gooberFillColour);
+    stroke(gooberOutlineColour);
+    strokeWeight(outlineWidth);
+    beginShape(); //top body
+    curveVertex (x-110, y+135);
+    curveVertex (x-110, y+135); //bottom left
+    curveVertex (x-100, y-65);
+    curveVertex (x, y-130); //middle
+    curveVertex (x+100, y-65);
+    curveVertex (x+110, y+135); //bottom right
+    curveVertex (x+110, y+135); 
+    endShape();
+    gooberSwoops (x-110, y+135); //left most 'swoop'
+    gooberSwoops (x-55, y+135);
+    gooberSwoops (x, y+135);
+    gooberSwoops (x+55, y+135);
+
+
+//draw face 
+    if (contemptFace === true) {
+        drawContemptFace (x, y-55);
+    }
+    if (happyFace === true) {
+        drawHappyFace (x, y-55);
+    }
+    if (sadFace === true) {
+        drawSadFace (x, y-55);
+    }
+    if (angryFace === true) {
+        drawAngryFace (x, y-55);
+    }
+}
+
+    function gooberSwoops (x,y){
+        strokeWeight(outlineWidth);
+        beginShape();
+        curveVertex (x, y);
+        curveVertex (x, y); //left 
+        curveVertex (x+5, y+5);
+        curveVertex (x+15, y+10);
+        curveVertex (x+27.5, y+12); //middle
+        curveVertex (x+40, y+10); 
+        curveVertex (x+50, y+5);
+        curveVertex (x+55, y); //right
+        curveVertex (x+55, y);
+        endShape();
     }
 
-
-    // ============================================================
-    //  SETUP
-    // ============================================================
-
-    function isMobile() { return window.innerWidth <= 768; }
-
-    function canvasSize() {
-        if (isMobile()) return { w: window.innerWidth, h: window.innerHeight };
-        return { w: p.windowWidth - 40, h: p.windowHeight - 40 };
+    function gooberColour (bodyOpacity, lineOpacity){
+    if (tired === true){ // #fccd77
+        gooberOutlineColour = [39, 53, 92, lineOpacity];
+        gooberFillColour = [39, 53, 99, bodyOpacity];
     }
-
-    p.setup = function() {
-        let sz  = canvasSize();
-        let cnv = p.createCanvas(sz.w, sz.h);
-        cnv.parent('canvas-container');
-        //cnv.mousePressed(onCanvasClick);
-
-        creature = createCreature(p.width / 2, p.height / 2);
-      //  loadState(creature);
-
-        // window.addEventListener('focus', () => { creature.isWatched = true; });
-        // window.addEventListener('blur',  () => { creature.isWatched = false; });
-
-        // setInterval(() => {
-        //     saveState(creature);
-        //     creature.hour = new Date().getHours();
-        // }, 30000);
-
-        // window.addEventListener('beforeunload', () => saveState(creature));
-    };
-
-
-    // ============================================================
-    //  DRAW LOOP
-    // ============================================================
-
-    p.draw = function() {
-        p.background(255);
-
-        let c = creature;
-
-
-
-        // Draw creature
-        drawCreature(c);
-    };
-
-
-    // ============================================================
-    //  DRAW CREATURE  —  replace this with your design!
-    // ============================================================
-
-    function drawCreature(c) {
-
-        p.push();
-        p.translate(c.x, c.y);
-
-        // Body
-        p.noStroke();
-        p.fill(0);
-        p.ellipse(0, 0, 200);
-
-        p.pop();
+    else if (angry === true){ // #fc7777
+        gooberOutlineColour = [0, 53, 92, lineOpacity];
+        gooberFillColour = [0, 53, 99, bodyOpacity];
     }
-
-
-    // ============================================================
-    //  INPUT: MOUSE CLICK  —  feed the creature
-    // ============================================================
-
-    // function onCanvasClick() {
-    //     if (!micActive) startMic();
-
-     
-    //     saveState(creature);
-    // }
-
-
-    // ============================================================
-    //  INPUT: MICROPHONE
-    // ============================================================
-
-    // async function startMic() {
-    //     try {
-    //         let stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
-    //         let ctx    = new (window.AudioContext || window.webkitAudioContext)();
-    //         let source = ctx.createMediaStreamSource(stream);
-    //         micAnalyser = ctx.createAnalyser();
-    //         micAnalyser.fftSize = 256;
-    //         source.connect(micAnalyser);
-    //         micData   = new Uint8Array(micAnalyser.frequencyBinCount);
-    //         micActive = true;
-    //     } catch(e) {
-    //         console.log('Mic unavailable:', e);
-    //     }
-    // }
-
-    // function getMicLevel() {
-    //     if (!micAnalyser) return 0;
-    //     micAnalyser.getByteFrequencyData(micData);
-    //     let sum = 0;
-    //     for (let i = 0; i < micData.length; i++) sum += micData[i];
-    //     return sum / (micData.length * 255);
-    // }
-
-    // function updateMic(c) {
-    //     if (!micActive) return;
-    //     c.micLevel = getMicLevel();
-    //     if (c.micLevel > MIC_THRESHOLD) c.exciteTimer = EXCITED_FRAMES;
-    // }
-
-
-    // ============================================================
-    //  PERSISTENCE
-    // ============================================================
-
-    function saveState(c) {
+    else if (annoyed === true){ // #d677fc
+        gooberOutlineColour = [283, 53, 92, lineOpacity];
+        gooberFillColour = [283, 53, 99, bodyOpacity];
+    }
+    else if (bored === true){ // #9f77fc
+        gooberOutlineColour = [258, 53, 92, lineOpacity];
+        gooberFillColour = [258, 53, 99, bodyOpacity];
+    }
+    else if (sad === true){ // #66a2fd
+        gooberOutlineColour = [216, 60, 92, lineOpacity];
+        gooberFillColour = [216, 60, 99, bodyOpacity];
+    }
+    else if (distracted === true){ // #80eafd
+        gooberOutlineColour = [189, 49, 92, lineOpacity];
+        gooberFillColour = [189, 49, 99, bodyOpacity];
+    }
+    else if (overwhelmed === true){ // #4ec9a4
+        gooberOutlineColour = [162, 61, 72, lineOpacity];
+        gooberFillColour = [162, 61, 79, bodyOpacity];
+    }
+    else if (panicked === true){ // #87c951
+        gooberOutlineColour = [93, 60, 72, lineOpacity];
+        gooberFillColour = [93, 60, 79, bodyOpacity];
+    }
+    else if (excited === true){ // #b5eb64
+        gooberOutlineColour = [91, 57, 85, lineOpacity];
+        gooberFillColour = [91, 57, 92, bodyOpacity];
+    }
+    else if (happy === true){ // #effc77
+        gooberOutlineColour = [66, 53, 92, lineOpacity];
+        gooberFillColour = [66, 53, 99, bodyOpacity];
+    }
+    else if (calm === true){ // #fff5de
+        gooberOutlineColour = [42, 13, 93, lineOpacity];
+        gooberFillColour = [42, 13, 100, bodyOpacity];
+    }
+    else {
+        gooberOutlineColour = [283, 2, 52, lineOpacity];
+        gooberFillColour = [283, 0, 59, bodyOpacity];
+    }
         
     }
-
-    function loadState(c) {
+    //faces
+    function drawContemptFace (x, y){
+        noFill();
+        stroke(faceColour);
+        strokeWeight(outlineWidth*2);
+        ellipse (x-45, y, 5, 12); //left eye
+        ellipse (x+45, y, 5, 12); //right eye
+        beginShape(); //mouth
+        strokeWeight(outlineWidth);
+        curveVertex (x-25, y+30);
+        curveVertex (x-25, y+30); // left
+        curveVertex (x-16, y+39);
+        curveVertex (x, y+44); //middle
+        curveVertex (x+16, y+39);
+        curveVertex (x+25, y+30); // right
+        curveVertex (x+25, y+30); 
+        endShape();
 
     }
+    function drawHappyFace (x, y){
+        noFill();
+        stroke(faceColour);
+        strokeWeight(outlineWidth);
+        line (x-55, y-5, x-30, y+5); //left eye
+        line ( x-30, y+5, x-55, y+10); 
+        line (x+55, y-5, x+30, y+5); //left eye
+        line ( x+30, y+5, x+55, y+10); 
+        beginShape(); //mouth
+        curveVertex (x-25, y+30);
+        curveVertex (x-25, y+30); // left
+        curveVertex (x-16, y+39);
+        curveVertex (x, y+44); //middle
+        curveVertex (x+16, y+39);
+        curveVertex (x+25, y+30); // right
+        curveVertex (x+25, y+30); 
+        endShape();
 
+    }
+    function drawSadFace (x, y){
+        noFill();
+        stroke(faceColour);
+        strokeWeight(outlineWidth*2);
+        ellipse (x-45, y, 5, 12); //left eye
+        ellipse (x+45, y, 5, 12); //right eye
+        beginShape(); //mouth
+        strokeWeight(outlineWidth);
+        curveVertex (x-25, y+38);
+        curveVertex (x-25, y+38); // left
+        curveVertex (x-16, y+29);
+        curveVertex (x, y+25); //middle
+        curveVertex (x+16, y+29);
+        curveVertex (x+25, y+38); // right
+        curveVertex (x+25, y+38); 
+        endShape();
+    }
+    function drawAngryFace (x, y){
+        noFill();
+        stroke(faceColour);
+        strokeWeight(outlineWidth*2);
+        ellipse (x-45, y, 5, 12); //left eye
+        ellipse (x+45, y, 5, 12); //right eye
+        strokeWeight(outlineWidth);
+        line (x-55, y-20, x-30, y-10); //eyebrows
+        line (x+55, y-20, x+30, y-10);
+        beginShape(); //mouth
+        curveVertex (x-25, y+38);
+        curveVertex (x-25, y+38); // left
+        curveVertex (x-16, y+29);
+        curveVertex (x, y+25); //middle
+        curveVertex (x+16, y+29);
+        curveVertex (x+25, y+38); // right
+        curveVertex (x+25, y+38); 
+        endShape();
+    }
 
-    // ============================================================
-    //  RESIZE
-    // ============================================================
+function mouseClicked () {
+// press on goober
+  if (showFeelingsMenu === false && showMainMenu === false && mouseX > gooberPosX - gooberWidth/2 && mouseX < gooberPosX + gooberWidth/2 &&
+      mouseY > gooberPosY - gooberHeight/2 && mouseY < gooberPosY + gooberHeight/2) {
+    console.log('Clicked on goober');
+    showMainMenu = true
+  }
+//press 'log feelings'
+  else if (showMainMenu === true && mouseX > gooberPosX+180 && mouseX < gooberPosX+340 && mouseY > gooberPosY-100 && mouseY < gooberPosY-60) {
+    console.log('Clicked on log feelings');
+    showMainMenu = false
+    showFeelingsMenu = true
+  }
+//calm
+  else if (showMainMenu === false && showFeelingsMenu === true && mouseX > gooberPosX+180 && mouseX < gooberPosX+305 && mouseY > gooberPosY-100 && mouseY < gooberPosY-80) {
+    console.log('Clicked on log feelings');
+    showMainMenu = false
+    showFeelingsMenu = true
+    calm = true
+    happy = false
+  }
+//happy
+  else if (showMainMenu === false && showFeelingsMenu === true && mouseX > gooberPosX+180 && mouseX < gooberPosX+305 && mouseY > gooberPosY-72 && mouseY < gooberPosY-62) {
+    console.log('Clicked on log feelings');
+    showMainMenu = false
+    showFeelingsMenu = true
+    calm = false
+    happy = true
+  }
+}
 
-    p.windowResized = function() {
-        let sz = canvasSize();
-        p.resizeCanvas(sz.w, sz.h);
-        creature.originX = p.width / 2;
-        creature.originY = p.height / 2;
-        creature.x = creature.originX;
-        creature.y = creature.originY;
-    };
+function drawMainMenu(x, y) {
+    fill(mainMenuFillColour);
+    stroke(mainMenuOutlineColour);
+    strokeWeight(mainMenuOutlineWidth);
 
-}, document.body);
+    //thinking bubbles
+    ellipse (x+110, y-110, 30, 30);
+    ellipse (x+137, y-130, 20, 20);
+    ellipse (x+160, y-140, 15, 15);
+    //menu bubbles
+    rect(x+180, y-160, 230, 50, 20); //hello!
+    rect(x+180, y-100, 160, 40, 20); //feeling
+    rect(x+180, y-50, 237, 40, 20); //tasks
+    rect(x+180, y, 165, 40, 20); //food
+    rect(x+180, y+50, 150, 40, 20); //help
+    rect(x+180, y+100, 260, 40, 20); //proud
+
+    //text
+    fill(textColour);
+    stroke(textColour)
+    strokeWeight (0.75)
+    textSize (20);
+    text ('Hello! What do you need?', x+195, y-127);
+    text ('Log a feeling •‿•', x+195, y-75);
+    text ('Share what you like to do', x+195, y-23);
+    text ('Share food 𓌉◯𓇋', x+195, y+27);
+    text ('Ask for Help ♡', x+195, y+77);
+    text ('Say Your Proud of Yourself', x+195, y+125);
+}
+
+function drawFeelingsMenu(x, y) {
+    fill(mainMenuFillColour);
+    stroke(mainMenuOutlineColour);
+    strokeWeight(mainMenuOutlineWidth);
+
+    //thinking bubbles
+    ellipse (x+110, y-110, 30, 30);
+    ellipse (x+137, y-130, 20, 20);
+    ellipse (x+160, y-140, 15, 15);
+    //menu bubbles
+    rect(x+180, y-160, 227, 50, 20); //hello!
+    //left column
+    rect(x+180, y-100, 125, 20, 20); //calm
+    rect(x+180, y-72, 125, 20, 20); //happy
+    rect(x+180, y-44, 125, 20, 20); //Excited
+    rect(x+180, y-16, 125, 20, 20); //Panicked
+    rect(x+180, y+12, 125, 20, 20); //overwhelmed
+    rect(x+180, y+40, 125, 20, 20); //distracted
+    //right column
+    rect(x+312, y-100, 95, 20, 20); //sad
+    rect(x+312, y-72, 95, 20, 20); //bored
+    rect(x+312, y-44, 95, 20, 20); //Annoyed
+    rect(x+312, y-16, 95, 20, 20); //Angry
+    rect(x+312, y+12, 95, 20, 20); //Tired
+    rect(x+312, y+40, 95, 20, 20); //Nothing
+    //OK
+    rect(x+180, y+70, 227, 50, 20); //done
+
+    //colour bubbles
+    //left column
+    push ();
+    strokeWeight(2.5);
+    translate (8, 4)
+    stroke(42, 13, 93);
+    fill(42, 13, 100);
+    rect(x+180, y-100, 12, 12, 5); //calm
+    stroke(66, 53, 92);
+    fill(66, 53, 99);
+    rect(x+180, y-72, 12, 12, 5); //happy
+    stroke(91, 57, 85);
+    fill(91, 57, 92);
+    rect(x+180, y-44, 12, 12, 5); //Excited
+    stroke(93, 60, 72);
+    fill(93, 60, 79);
+    rect(x+180, y-16, 12, 12, 5); //Panicked
+    stroke(162, 61, 72);
+    fill(162, 61, 79);
+    rect(x+180, y+12, 12, 12, 5); //overwhelmed
+    stroke(189, 49, 92);
+    fill(189, 49, 99);
+    rect(x+180, y+40, 12, 12, 5); //distracted
+    //right column
+    rect(x+312, y-100, 12, 12, 5); //sad
+    rect(x+312, y-72, 12, 12, 5); //bored
+    rect(x+312, y-44, 12, 12, 5); //Annoyed
+    rect(x+312, y-16, 12, 12, 5); //Angry
+    rect(x+312, y+12, 12, 12, 5); //Tired
+    rect(x+312, y+40, 12, 12, 5); //Nothing
+    pop ();
+
+    //text
+    fill(textColour);
+    stroke(textColour)
+    strokeWeight (0.75)
+    textSize (19);
+    text ('How are you feeling today?', x+185, y-127);
+    text ('Yup Thats me', x+240, y+100);
+     textSize (17);
+    //left column
+    text ('Calm', x+205, y-85);
+    text ('Happy', x+205, y-58);
+    text ('Excited', x+205, y-27);
+    text ('Panicked', x+205, y);
+    text ('Overwhelmed', x+205, y+27);
+    text ('Distracted', x+205, y+55);
+    //right column
+    text ('Sad', x+337, y-85);
+    text ('Bored', x+337, y-57);
+    text ('Annoyed', x+337, y-27);
+    text ('Angry', x+337, y-1);
+    text ('Tired', x+337, y+27);
+    text ('Nothing', x+337, y+54);
+}
+
+// Resize the canvas if the window is resized
+function windowResized() {
+  const { w, h } = canvasSize();
+  resizeCanvas(w, h);
+}
